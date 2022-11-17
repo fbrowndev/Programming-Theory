f9203c6db8d9100707bc5a5bdacfff8447caeb6d
Keke Brown <fbrowndev@gmail.com>
2022-11-17 10:50:05 -0500

    Main Menu Finalized
    Basic Functionality added

--- Assets/Scritps/MainMenu/MainMenuHandler.cs
+++ Assets/Scritps/MainMenu/MainMenuHandler.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
+/// <summary>
+/// Main Menu functionality
+/// </summary>
 public class MainMenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Main Menu Event Handlers
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+    Application.Quit();
+#endif
     }
+    #endregion
 }
