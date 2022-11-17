ad4686ef26f2e99c1264cc7a1cfa82b96854e73f
Keke Brown <fbrowndev@gmail.com>
2022-11-17 12:11:42 -0500

    Animations Created
    Basic Player Animation and parameters added

---
+++ Assets/Scritps/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Controlling all functionality of the player
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+}
