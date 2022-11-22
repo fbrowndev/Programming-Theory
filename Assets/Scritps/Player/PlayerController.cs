f791b356e6c85225fcdf73f9090d71b2f5d5e35c
Keke Brown <fbrowndev@gmail.com>
2022-11-22 00:38:42 -0500

    Basic Movement
    Character movement without animations or gravity effects

--- Assets/Scritps/Player/PlayerController.cs
+++ Assets/Scritps/Player/PlayerController.cs
@@ -8,18 +8,62 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
+    #region Player Controller Variables
+
+    //Variables
+    [Header("Speed controls")]
+    [SerializeField] float runSpeed;
+
+    Vector3 moveDirection;
+    Vector3 velocity;
+
+    [Header("Ground Check")]
+    [SerializeField] bool isGrounded;
+    [SerializeField] float groundCheckDistance;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float gravity;
+
+    //References
+    CharacterController controller;
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+    }
+
+    #region Player Movement
+    void PlayerMovement()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(horizontalMovement, 0, verticalMovement) * runSpeed * Time.deltaTime;
+
+        controller.Move(moveDirection);
         
     }
 
-    
+    void PlayerJump()
+    {
+        //Add code here later
+    }
+
+    void PlayerFire()
+    {
+        //Add Code here later
+    }
+
+    #endregion
 
 }
